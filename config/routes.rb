296a6e57172e2e6dcd4d6582725098ec50eac7fc
Nathan Khuu <nkhuu@berkeley.edu>
2017-04-23 15:44:34 -0700

    working on articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   get '/entries/new', to: 'entries#new'
-   get '/entries/show', to: 'entries#show'
+  get '/entries/show', to: 'entries#show'
   post '/entries/new', to: 'entries#create'
   get 'visitors/index', to: 'visitors#index'
+  get 'visitors/alcohol', to: 'visitors#alcohol'
+  get 'visitors/hygiene', to: 'visitors#hygiene'
   get '/users/index', to: 'users#index'
   get '/users/calendar', to: 'users#calendar'
 end
